e64a6c313c6f7047e474ac378f3f97c3121bc156
Dany Diaz <dany.diaz91@hotmail.com>
2020-06-25 21:51:36 -0500

    feat: add file argument to allow another cat pictures

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,12 @@ struct Options {
     message: String,
 
     #[structopt(short = "d", long = "dead")]
-    // Make the cat appear dead.
+    /// Make the cat appear dead.
     dead: bool,
+
+    #[structopt(short = "f", long = "file", parse(from_os_str))]
+    /// Load the cat picture from the specified file
+    catfile: Option<std::path::PathBuf>,
 }
 
 fn main() {
@@ -20,9 +24,21 @@ fn main() {
     let eye = if options.dead { "x" } else { "o" };
 
     println!("{}", message.bright_yellow().underline().on_purple());
-    println!("  \\");
-    println!("   \\");
-    println!("     /\\_/\\");
-    println!("    ( {eye} {eye} )", eye=eye.red().bold());
-    println!("    =( I )=");
+
+
+    match &options.catfile {
+        Some(path) => {
+            let cat_template = std::fs::read_to_string(path)
+                .expect(&format!("could not read file {:?}", path));
+            let cat_picture = cat_template.replace("{eye}", eye);
+            println!("{}", &cat_picture);
+        },
+        None => {
+            println!("  \\");
+            println!("   \\");
+            println!("     /\\_/\\");
+            println!("    ( {eye} {eye} )", eye=eye.red().bold());
+            println!("    =( I )=");        
+        }
+    }    
 }
